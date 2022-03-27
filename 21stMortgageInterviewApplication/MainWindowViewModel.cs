be8637d0db51939010fcb3886658ae461626c74c
J. B. Lopez <lopezcoder@gmail.com>
2022-03-27 15:23:02 -0700

    Interview project for 21st Mortgage
    Commit after 90 minute time limit expired.

---
+++ 21stMortgageInterviewApplication/MainWindowViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel;
+
+namespace _21stMortgageInterviewApplication
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private NumberListModel _NumberListModel;
+
+        private string _UserInput;
+        public string UserInput
+        {
+            get { return _UserInput;}
+            set
+            {
+                _UserInput = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("UserInput"));
+            }
+        }
+
+        private string _Output;
+        public string Output
+        {
+            get { return _Output; }
+            set
+            {
+                _Output = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Output"));
+            }
+        }
+
+        private RelayCommand _FindLargeNumberCommand;
+        public RelayCommand FindLargeNumberCommand
+        {
+            get { return _FindLargeNumberCommand; }
+            set
+            {
+                _FindLargeNumberCommand = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FindLargeNumberCommand"));
+            }
+        }
+
+        private RelayCommand _FindSumOfOddNumbersCommand;
+        public RelayCommand FindSumOfOddNumbersCommand
+        {
+            get { return _FindSumOfOddNumbersCommand; }
+            set
+            {
+                _FindSumOfOddNumbersCommand = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FindSumOfOddNumbersCommand"));
+            }
+        }
+
+        private RelayCommand _FindSumOfEvenNumbersCommand;
+        public RelayCommand FindSumOfEvenNumbersCommand
+        {
+            get { return _FindSumOfEvenNumbersCommand; }
+            set
+            {
+                _FindSumOfEvenNumbersCommand = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FindSumOfEvenNumbersCommand"));
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            _NumberListModel            = new NumberListModel();
+            FindLargeNumberCommand      = new RelayCommand(CommandFindLargestNumberExecute, CommandFindLargestNumberCanExecute);
+            FindSumOfOddNumbersCommand  = new RelayCommand(CommandFindSumOfOddNumbersExecute, CommandFindSumOfOddNumbersCanExecute);
+            FindSumOfEvenNumbersCommand = new RelayCommand(CommandFindSumOfEvenNumbersExecute, CommandFindSumOfEvenNumbersCanExecute);
+        }
+
+        private bool CommandFindLargestNumberCanExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            return _NumberListModel.GetNumberCount() > 0;
+        }
+
+        private void CommandFindLargestNumberExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            Output = _NumberListModel.GetLargestNumber().ToString();
+        }
+
+        private bool CommandFindSumOfOddNumbersCanExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            return _NumberListModel.GetNumberCount() > 0;
+        }
+
+        private void CommandFindSumOfOddNumbersExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            Output = _NumberListModel.GetSumOfOddNumbers().ToString();
+        }
+
+        private bool CommandFindSumOfEvenNumbersCanExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            return _NumberListModel.GetNumberCount() > 0;
+        }
+
+        private void CommandFindSumOfEvenNumbersExecute(object parameter)
+        {
+            _NumberListModel.FromString(parameter as string);
+            Output = _NumberListModel.GetSumOfEvenNumbers().ToString();
+        }
+    }
+}
---
+++ 21stMortgageInterviewApplication/NumberListModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _21stMortgageInterviewApplication
+{
+    public class NumberListModel
+    {
+        public List<int> Numbers { get; set; }
+
+        public NumberListModel()
+        {
+            Numbers = new List<int>();
+        }
+
+        public string FromString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            try
+            {
+                Numbers = input.Split(',').Select(int.Parse).ToList();
+            }
+            catch (Exception e)
+            {
+                return "Enter a valid list of numbers separated by commas";
+            }
+
+            return null;
+        }
+
+        public void AddNumber(int number)
+        {
+            Numbers.Add(number);
+        }
+
+        public void RemoveNumber(int number)
+        {
+            Numbers.Remove(number);
+        }
+
+        public void RemoveAllNumbers()
+        {
+            Numbers.Clear();
+        }
+
+        public int GetNumberCount()
+        {
+            return Numbers.Count;
+        }
+
+        public int GetLargestNumber()
+        {
+            return Numbers.Max();
+        }
+
+        public int GetSmallestNumber()
+        {
+            return Numbers.Min();
+        }
+
+        public int GetSumOfEvenNumbers()
+        {
+            return Numbers.Where(x => x % 2 == 0).Sum();
+        }
+
+        public int GetSumOfOddNumbers()
+        {
+            return Numbers.Where(x => x % 2 != 0).Sum();
+        }
+
+        public int GetSumOfAllNumbers()
+        {
+            return Numbers.Sum();
+        }
+
+    }
+}
